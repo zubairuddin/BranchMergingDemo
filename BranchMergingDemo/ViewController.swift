d90ac28140a7632b36ccd66ef33853cf331c0c7c
Zubair <zubairuddin33@gmail.com>
2019-03-12 21:37:07 +0530

    Initial Commit

---
+++ BranchMergingDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BranchMergingDemo
+//
+//  Created by Zubair on 12/03/19.
+//  Copyright © 2019 applligent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
